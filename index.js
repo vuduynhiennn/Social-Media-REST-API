866821ae533805c3ac58425c51948b72480c7e09
nhien <vuduynhiencute@gmail.com>
2022-01-24 15:49:20 +0700

    adsf

--- index.js
+++ index.js
@@ -4,13 +4,33 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
-dotenv.config()
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=> {
-    console.log("connected to mongoDB")
+    console.log("connected to mongoDB");
 });
 
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+app.get("/", (req, res) => {
+    res.send("welcome to home page and I'm Vu Duy Nhien a profestional programmerm");
+})
+
+
+app.get("/user", (req, res) => {
+    res.send("this is user home page");
+})
+
+
+
+
 app.listen(8800, () => {
-    console.log("backend server is fujdhsfhaskdhjcking")
+    console.log("backend server is running on port 8800");
 })
---
+++ routes/auth.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+// register
+
+router.post("/register", async (req, res) => {
+//     const user = await new User({
+//         username: "john",
+//         email: "nhienduyvu@gmail.com",
+//         password: "1234567"
+//     });
+//     await user.save();
+//     res.send("ok");
+//  
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        req.status(500).json(err);
+        
+    }
+})
+
+//login
+router.post("/login", (req, res) =>{
+    try {
+        const user = User.findOne({email:req.body.email});
+        !user && res.status(404).send("user not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("wrong password");
+        res.status(200).json(user);
+    } catch(err) {
+        req.status(500).json(err);
+    }
+})
+
+//update user
+
+module.exports = router;
